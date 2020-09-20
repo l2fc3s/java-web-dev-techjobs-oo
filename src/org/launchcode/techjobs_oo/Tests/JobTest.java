1287b5f1c19ab879fd4e391fb1196d962a62528c
Larry LaMar <lmrlrry@gmail.com>
2020-09-20 17:00:11 -0500

    completed two tests

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,40 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
+
+
+public class JobTest {
+    public Job testJob1;
+    public Job testJob2;
+    @Before
+    public void jobObjects(){
+          testJob1 = new Job();
+          testJob2 = new Job();
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertEquals(testJob2.getId() - testJob1.getId(),1);
+//        System.out.println(testJob1.getId());
+//        System.out.println(testJob2.getId());
+    }
+
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job constructorTest = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals(constructorTest instanceof Job,true);
+        assertEquals(constructorTest.getName() instanceof String, true);
+        assertEquals(constructorTest.getEmployer().toString() instanceof String, true);
+        assertEquals(constructorTest.getLocation().toString() instanceof String,true);
+        assertEquals(constructorTest.getPositionType().toString() instanceof String,true);
+        assertEquals(constructorTest.getCoreCompetency().toString() instanceof String,true);
+        //System.out.println(constructorTest.getEmployer().toString() instanceof String);
+    }
+
+}
